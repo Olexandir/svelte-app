744c7fc67947e943a206bcc5c4de21840d44925a
Alexander Davidenko <gypsyman534@gmail.com>
2023-08-10 12:57:35 +0300

    image-gallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,93 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import axios from 'axios';
+	import { onMount, onDestroy, afterUpdate, beforeUpdate } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+
+	let term = '';
+	let photos: {
+		id: string;
+		alt_descriptions: string;
+		urls: { regular: string };
+	}[] = [];
+
+	const fetchData = async () => {
+		const response = await axios.get(
+			`https://api.unsplash.com/search/photos?page=1&query=${
+				term || 'office'
+			}&client_id=LkttVpC7o3ZgxMkGYb36dZNbWSToFa1dOwxAwbgRiPM`
+		);
+		photos = response.data.results;
+	};
+
+	onMount(() => {
+		fetchData();
+	});
+
+	const handleSearch = async () => {
+		if (!term) return;
+		await fetchData();
+		term = '';
+	};
+</script>
+
+<div class="container">
+	<div class="header">
+		<h1>Image Gallery</h1>
+		<div class="input-container">
+			<button class="button" on:click={() => (term = '')}>clear</button>
+			<input type="text" class="input" bind:value={term} />
+			<button class="button" on:click={handleSearch}>Search</button>
+		</div>
+	</div>
+
+	<div class="photos">
+		{#each photos as photo, i (photo.id)}
+			<img
+				src={photo.urls.regular}
+				alt={photo.alt_descriptions}
+				class="image"
+				in:fly={{ y: 200, duration: 1500, delay: i * 200 }}
+				out:fade
+			/>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.image {
+		width: 400px;
+		height: 250px;
+		margin: 5px;
+	}
+	.photos {
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.container {
+		width: 1230px;
+		margin: 0 auto;
+	}
+	.header {
+		text-align: center;
+		font-size: 20px;
+	}
+	.input {
+		padding: 10px;
+		width: 400px;
+		border-radius: 10px;
+		outline: none;
+		border: 1px solid gray;
+		font-size: 20px;
+	}
+	.button {
+		padding: 10px;
+		font-size: 20px;
+		background-color: rebeccapurple;
+		border-radius: 10px;
+		border: none;
+		color: white;
+	}
+	.input-container {
+		margin-bottom: 40px;
+	}
+</style>
